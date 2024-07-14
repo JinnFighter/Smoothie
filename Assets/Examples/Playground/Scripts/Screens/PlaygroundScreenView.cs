2fb680238ea899a67403f91ce22f1c0604cc170b
JinnFighter <jinnfighterwork@yandex.ru>
2024-07-14 19:05:40 +0300

    add: button to playgroun screen view

--- Assets/Examples/Playground/Scripts/Screens/PlaygroundScreenView.cs
+++ Assets/Examples/Playground/Scripts/Screens/PlaygroundScreenView.cs
@@ -1,9 +1,11 @@
 ﻿using Smoothie.Scripts.Widgets;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Examples.Playground.Scripts.Screens
 {
     public class PlaygroundScreenView : BaseView
     {
-        
+        [field: SerializeField] public Button ButtonCreateDialog { get; private set; }
     }
 }
--- Assets/Examples/Playground/Scripts/Screens/PlaygroundUiScreen.cs
+++ Assets/Examples/Playground/Scripts/Screens/PlaygroundUiScreen.cs
@@ -4,5 +4,18 @@ namespace Examples.Playground.Scripts.Screens
 {
     public class PlaygroundUiScreen : UiScreen<PlaygroundScreenViewModel, PlaygroundScreenView>
     {
+        protected override void InitInner()
+        {
+            View.ButtonCreateDialog.onClick.AddListener(HandleButtonCreateDialogClicked);
+        }
+
+        protected override void TerminateInner()
+        {
+            View.ButtonCreateDialog.onClick.RemoveListener(HandleButtonCreateDialogClicked);
+        }
+
+        private void HandleButtonCreateDialogClicked()
+        {
+        }
     }
 }
